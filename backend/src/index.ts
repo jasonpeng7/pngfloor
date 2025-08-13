f7974b09d9490c3e685ddbc4a21ce59c3f7f38d2
jasonpeng7 <144397187+jasonpeng7@users.noreply.github.com>
2025-08-13 16:33:20 -0700

    start of sum great

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,22 @@
 import { Hono } from "hono";
-import { drizzle } from "drizzle-orm/node-postgres";
-import "dotenv/config";
+import { cors } from "hono/cors";
 
 const app = new Hono();
-const db = drizzle(process.env.DATABASE_URL!);
 
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    allowMethods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+// Root route
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
 
-export default app;
+export default {
+  port: 8080,
+  fetch: app.fetch,
+};
