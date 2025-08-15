8f925fab26f9a7c05f0bd9449a3775bf72dd3d9f
jasonpeng7 <144397187+jasonpeng7@users.noreply.github.com>
2025-08-15 01:21:11 -0700

    updated mount routes, added cancel booking route

--- backend/src/routes/bookings.ts
+++ backend/src/routes/bookings.ts
@@ -4,6 +4,8 @@ import {
   createBooking,
   updateBooking,
   deleteBooking,
+  getBookingsByCustomerId,
+  cancelBooking,
 } from "../controllers/bookings-controllers";
 
 import { Hono } from "hono";
@@ -11,9 +13,11 @@ import { Hono } from "hono";
 const bookingsRouter = new Hono();
 
 bookingsRouter.get("/", ...getBookings);
+bookingsRouter.get("/user/:customerId", ...getBookingsByCustomerId);
 bookingsRouter.get("/:id", ...getBookingById);
 bookingsRouter.post("/", ...createBooking);
 bookingsRouter.put("/:id", ...updateBooking);
 bookingsRouter.delete("/:id", ...deleteBooking);
+bookingsRouter.delete("/:id/cancel/:customerId", ...cancelBooking);
 
 export default bookingsRouter;
