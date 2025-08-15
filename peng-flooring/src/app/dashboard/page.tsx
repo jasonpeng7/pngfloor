8eb7882f9644032bc2e793e5e0a990873b9771d9
jasonpeng7 <144397187+jasonpeng7@users.noreply.github.com>
2025-08-15 01:25:02 -0700

    chore

--- peng-flooring/src/app/dashboard/page.tsx
+++ peng-flooring/src/app/dashboard/page.tsx
@@ -121,7 +121,7 @@ export default function DashboardPage() {
   }
 
   return (
-    <div className="min-h-screen bg-gray-50 py-8 inter-tight-regular">
+    <div className="max-w-[2000px] min-h-screen bg-gray-50 py-8 inter-tight-regular">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="bg-white p-6">
           <h1 className="text-3xl font-bold text-gray-900 mb-6">
