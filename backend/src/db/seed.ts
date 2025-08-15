9b399b32dec0c12f50351d37ad2f62188b470a30
jasonpeng7 <144397187+jasonpeng7@users.noreply.github.com>
2025-08-14 18:35:43 -0700

    updated seeding script

--- backend/src/db/seed.ts
+++ backend/src/db/seed.ts
@@ -6,8 +6,9 @@ import { ulid } from "ulid";
 const main = async () => {
   console.log("seeding database...");
 
-  await db.delete(customers);
+  // delete in correct order to respect foreign key constraints
   await db.delete(bookings);
+  await db.delete(customers);
 
   const customer1Id = ulid();
 
@@ -25,6 +26,14 @@ const main = async () => {
     id: ulid(),
     customer_id: customer1Id,
     date: new Date(),
+    name: "John Doe",
+    email: "john.doe@example.com",
+    address: "123 Main St, Anytown, USA",
+    phone_number: "6281234567890",
+    house_size: 2000,
+    rooms: "2",
+    service: "Free Estimate",
+    message: "I would like to get an estimate for my home",
     status: "pending",
   });
 
