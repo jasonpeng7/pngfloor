ff94515a17b3dc624b43ee9547abdaad72dd5298
jasonpeng7 <144397187+jasonpeng7@users.noreply.github.com>
2025-08-15 01:16:55 -0700

    added more icons

--- peng-flooring/src/components/icons.tsx
+++ peng-flooring/src/components/icons.tsx
@@ -110,7 +110,207 @@ export const CustomIcon = ({
   <svg className={className} fill={fill} viewBox="0 0 20 20">
     <path
       fillRule="evenodd"
+      d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+
+export const UserIcon = ({
+  className = "w-6 h-6",
+  fill = "currentColor",
+}: {
+  className?: string;
+  fill?: string;
+}) => (
+  <svg className={className} fill={fill} viewBox="0 0 20 20">
+    <path
+      fillRule="evenodd"
+      d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+
+export const AwardIcon = ({
+  className = "w-6 h-6",
+  fill = "currentColor",
+}: {
+  className?: string;
+  fill?: string;
+}) => (
+  <svg className={className} fill={fill} viewBox="0 0 20 20">
+    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+  </svg>
+);
+
+export const MapPinIcon = ({
+  className = "w-6 h-6",
+  fill = "currentColor",
+}: {
+  className?: string;
+  fill?: string;
+}) => (
+  <svg className={className} fill={fill} viewBox="0 0 20 20">
+    <path
+      fillRule="evenodd"
+      d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+
+export const CalendarIcon = ({
+  className = "w-6 h-6",
+  fill = "currentColor",
+}: {
+  className?: string;
+  fill?: string;
+}) => (
+  <svg className={className} fill={fill} viewBox="0 0 20 20">
+    <path
+      fillRule="evenodd"
+      d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+
+export const HeartIcon = ({
+  className = "w-6 h-6",
+  fill = "currentColor",
+}: {
+  className?: string;
+  fill?: string;
+}) => (
+  <svg className={className} fill={fill} viewBox="0 0 20 20">
+    <path
+      fillRule="evenodd"
+      d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+
+export const TrashIcon = ({
+  className = "w-6 h-6",
+  fill = "currentColor",
+}: {
+  className?: string;
+  fill?: string;
+}) => (
+  <svg className={className} fill={fill} viewBox="0 0 20 20">
+    <path
+      fillRule="evenodd"
+      d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+
+export const PaintBrushIcon = ({
+  className = "w-6 h-6",
+  fill = "currentColor",
+}: {
+  className?: string;
+  fill?: string;
+}) => (
+  <svg className={className} fill={fill} viewBox="0 0 20 20">
+    <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
+  </svg>
+);
+
+export const HammerIcon = ({
+  className = "w-6 h-6",
+  fill = "currentColor",
+}: {
+  className?: string;
+  fill?: string;
+}) => (
+  <svg className={className} fill={fill} viewBox="0 0 20 20">
+    <path
+      fillRule="evenodd"
       d="M11.3 1.046A1 1 0 0112 2v5h4a1 1 0 01.82 1.573l-7 10A1 1 0 018 18v-5H4a1 1 0 01-.82-1.573l7-10a1 1 0 011.12-.38z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+
+export const RulerIcon = ({
+  className = "w-6 h-6",
+  fill = "currentColor",
+}: {
+  className?: string;
+  fill?: string;
+}) => (
+  <svg className={className} fill={fill} viewBox="0 0 20 20">
+    <path
+      fillRule="evenodd"
+      d="M3 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+
+export const SparklesIcon = ({
+  className = "w-6 h-6",
+  fill = "currentColor",
+}: {
+  className?: string;
+  fill?: string;
+}) => (
+  <svg className={className} fill={fill} viewBox="0 0 20 20">
+    <path
+      fillRule="evenodd"
+      d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+
+export const BroomIcon = ({
+  className = "w-6 h-6",
+  fill = "currentColor",
+}: {
+  className?: string;
+  fill?: string;
+}) => (
+  <svg className={className} fill={fill} viewBox="0 0 20 20">
+    <path
+      fillRule="evenodd"
+      d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+
+export const StairsIcon = ({
+  className = "w-6 h-6",
+  fill = "currentColor",
+}: {
+  className?: string;
+  fill?: string;
+}) => (
+  <svg className={className} fill={fill} viewBox="0 0 20 20">
+    <path
+      fillRule="evenodd"
+      d="M3 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+
+export const BaseboardIcon = ({
+  className = "w-6 h-6",
+  fill = "currentColor",
+}: {
+  className?: string;
+  fill?: string;
+}) => (
+  <svg className={className} fill={fill} viewBox="0 0 20 20">
+    <path
+      fillRule="evenodd"
+      d="M3 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
       clipRule="evenodd"
     />
   </svg>
