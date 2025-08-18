be1c8f03fd4ec204f177d9b2b73f89f17e41de22
jasonpeng7 <144397187+jasonpeng7@users.noreply.github.com>
2025-08-18 01:56:44 -0700

    use workers compatible pg client

--- backend/src/db/db.ts
+++ backend/src/db/db.ts
@@ -1,17 +1,8 @@
-import { drizzle } from "drizzle-orm/node-postgres";
-import * as genericSchema from "./schema/generic";
-import { customers } from "./schema/customers";
-import { bookings } from "./schema/bookings";
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
 import { appEnv } from "../types/env";
 
-const pg_ssl =
-  appEnv.POSTGRES_SSL === "false" ? "disable" : appEnv.POSTGRES_SSL;
-
-const connectionString = `postgresql://${appEnv.POSTGRES_USER}:${appEnv.POSTGRES_PASSWORD}@${appEnv.POSTGRES_HOST}:${appEnv.POSTGRES_PORT}/${appEnv.POSTGRES_DB}?sslmode=${pg_ssl}`;
-const db = drizzle(connectionString, {
-  schema: {
-    ...genericSchema,
-  },
-});
+const sql = neon(appEnv.DATABASE_URL);
+const db = drizzle(sql);
 
 export default db;
